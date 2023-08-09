c91b60f520b96f843e855cc671b9a5da62f3160d
Chu <Chu@github.com>
2023-08-09 16:41:34 +0100

    cypress testing

--- cypress/e2e/MorseCode.cy.ts
+++ cypress/e2e/MorseCode.cy.ts
@@ -1,5 +1,27 @@
-describe("template spec", () => {
-  it("passes", () => {
-    cy.visit("https://example.cypress.io");
+
+describe("Morse Code Translator", () => {
+
+  beforeEach(() => {
+    cy.visit("https://koji47.github.io/morse-code-tech-test/");
   });
+  
+  it("Translates English to Morse code", () => {
+    cy.get("input").type("Hello World");
+    cy.get("button").click();
+    cy.get("#output").should("have.text", ".... . .-.. .-.. --- / .-- --- .-. .-.. -..")
+  });
+
+  it("Translates English to Morse code no matter the letter casing", () => {
+    cy.get("input").type("HeLLo woRLD");
+    cy.get("button").click();
+    cy.get("#output").should("have.text", ".... . .-.. .-.. --- / .-- --- .-. .-.. -..")
+  });
+
+
+  it("Translates English to Morse code and handle multiple words", () => {
+    cy.get("input").type("Hello world my name is chu");
+    cy.get("button").click();
+    cy.get("#output").should("have.text", ".... . .-.. .-.. --- / .-- --- .-. .-.. -.. / -- -.-- / -. .- -- . / .. ... / -.-. .... ..-")
+  });
+
 });
